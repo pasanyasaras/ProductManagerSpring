e74cab99131ccd63233eaedfaf7dd22c70a45324
pasanyasaras <pasansmart@gmail.com>
2020-12-18 21:00:56 +0530

    latest commit

---
+++ src/test/java/com/pasanyasara/ProductManager/ProductManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pasanyasara.ProductManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
